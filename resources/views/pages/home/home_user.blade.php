f6c422f6e744359ab8aa9deee42be9701c13aa89
alfajri23 <123190088@student.upnyk.ac.id>
2022-06-27 16:04:44 +0700

    home

--- resources/views/pages/home/home_user.blade.php
+++ resources/views/pages/home/home_user.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.layout_user')
 @section('content')
 
-<section class="main-banner" id="top">
-    <div class="container">
+<div class="banner-wrapper vh-md-100 layer-after app-shape">
+  <div class="container">
       <div class="row">
-        <div class="col-lg-6 align-self-center">
-          <div class="header-text">
-            <h6>Welcome to our school</h6>
-            <h2>Best Place To Learn Graphic <em>Design!</em></h2>
-            <div class="main-button-gradient">
-              <div class="scroll-to-section"><a href="#contact-section">Join Us Now!</a></div>
-            </div>
+          <div class="col-xxxl-7 col-xl-7 vh-md-100 pt-7 pb-7 align-items-center d-flex">
+              <div class="card w-100 border-0 bg-transparent">
+                  <h2 class="fw-700 text-grey-800 display5-size display4-xs-size lh-1 mb-0 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500">
+                    # Semua Orang <br>Berhak <span class="text-green">Bahagia</span>
+                  </h2>
+                  <h3 class="font-xl font-md-xs mt-4 text-grey-700 fw-400">
+                    Mari temukan kebahagiaanmu bersama kami<br>
+                    Menuju Indonesia bahagia 100%
+                  </h3>
+                  
+              </div>
           </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="right-image">
-            <img src="assets/images/banner-right-image.png" alt="">
+          <div class="col-xxxl-5 col-xl-4 vh-md-100 align-items-center d-flex ">
+              <div class="card w-100 border-0 bg-transparent text-center d-block">
+                  <img src="https://images.unsplash.com/photo-1524293568345-75d62c3664f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=411&q=80" alt="app-bg" class="w-100 rounded-xl os-init" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="500">    
+              </div>
           </div>
-        </div>
       </div>
-    </div>
-</section>
+  </div>
+</div>
 
 @endsection
