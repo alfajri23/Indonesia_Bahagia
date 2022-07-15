6cf7ecc12acd2ba882799d3fce5c391384f33c3f
alfajri23 <123190088@student.upnyk.ac.id>
2022-07-15 10:11:52 +0700

    ubah gitignore agar css js ke upload

---
+++ public/js/admin/dashboard/dashboard-3.js
@@ -0,0 +1,20 @@
+(function($) {
+    "use strict";
+
+    new dezSettings({
+        typography: "roboto",
+        version: "light",
+        layout: "Vertical",
+        headerBg: "color_1",
+        navheaderBg: "color_11",
+        sidebarBg: "color_1",
+        sidebarStyle: "full",
+        sidebarPosition: "fixed",
+        headerPosition: "fixed",
+        containerLayout: "full",
+        direction: "ltr"
+    }); 	
+	
+})(jQuery);
+
+/* const wt = new PerfectScrollbar('.widget-todo'); */
