57e80a2cc1473ae0c6ef0aab64a7272f6234549f
alfajri23 <123190088@student.upnyk.ac.id>
2022-07-18 11:37:07 +0700

    container

--- resources/views/pages/informasi/term_condition.blade.php
+++ resources/views/pages/informasi/term_condition.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
 
-    <div class="container p-5">
+    <div class="container p-3">
         <div class="">
             <h2 class="fw-700 text-grey-800 display2-size display3-md-size lh-3 pt-lg--5 text-center mb-5"></h2>
         </div class="fw-400 font-xss lh-28 text-grey-700 mt-0 ml-0 pl-3 w-100 w-xs-90 rich-list">
